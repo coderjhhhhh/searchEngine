1a52f73c392fcd3495b78b9f8a929c5c6b730a8e
goodFancier <nikitin743@gmail.com>
2018-10-10 22:24:05 +0400

    Дополнение предыдущего коммита

---
+++ src/main/java/com/goodfancier/searchEngine/Entity/Owner.java
@@ -0,0 +1,21 @@
+package com.goodfancier.searchEngine.Entity;
+
+public class Owner
+{
+    private String name;
+
+    public Owner(String name)
+    {
+        this.name = name;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/goodfancier/searchEngine/Entity/QueryResult.java
@@ -0,0 +1,13 @@
+package com.goodfancier.searchEngine.Entity;
+
+import java.util.List;
+
+public class QueryResult
+{
+    private List<Theme> items;
+
+    public List<Theme> getItems()
+    {
+        return items;
+    }
+}
