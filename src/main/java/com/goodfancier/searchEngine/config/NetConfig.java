692dc738cc80a016e60b6a70d18c3e785a319c76
goodFancier <nikitin743@gmail.com>
2018-10-14 10:35:54 +0400

    файл конфигурации

--- src/main/java/com/goodfancier/searchEngine/config/NetConfig.java
+++ src/main/java/com/goodfancier/searchEngine/config/NetConfig.java
@@ -2,13 +2,14 @@ package com.goodfancier.searchEngine.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * Find net configuration
  */
 @Configuration
-@EnableAutoConfiguration
+@ConfigurationProperties("netconfig")
 public class NetConfig
 {
     @Value("${net.host}")
