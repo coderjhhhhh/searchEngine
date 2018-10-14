4fc0dda8db0e159e202d5ebc677f6aaa92c26dfe
goodFancier <nikitin743@gmail.com>
2018-10-14 10:26:34 +0400

    изменил pom

--- src/main/java/com/goodfancier/searchEngine/SearchEngineApplication.java
+++ src/main/java/com/goodfancier/searchEngine/SearchEngineApplication.java
@@ -2,8 +2,10 @@ package com.goodfancier.searchEngine;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
 public class SearchEngineApplication
 {
     public static void main(String[] args)
